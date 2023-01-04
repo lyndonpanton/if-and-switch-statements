f947f2343fca8a3dd62c8f1ac5184b3d9c9e2d01
Lyndon Panton <LyndonPanton@gmail.com>
2023-01-04 22:09:42 +0000

    Completed problem 1
    Displayed menu options
    Retrieved user input
    Created if statement which operates based on the user input
    Displayed program close message

--- Exercise2/Exercise2/Program.cs
+++ Exercise2/Exercise2/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("**************");
+            Console.WriteLine();
+            Console.WriteLine("Menu: ");
+            Console.WriteLine("1 - New Game");
+            Console.WriteLine("2 - Load Game");
+            Console.WriteLine("3 - Options");
+            Console.WriteLine("4 - Quit");
+            Console.WriteLine();
+            Console.WriteLine("**************");
+            Console.WriteLine();
+
+            Console.Write("Please enter your choice (1 - 4): ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            if (option == 1)
+            {
+                Console.WriteLine("Creating a new game...");
+            } else if (option == 2)
+            {
+                Console.WriteLine("Loading the saved game...");
+            } else if (option == 3)
+            {
+                Console.WriteLine("Opening the options menu...");
+            } else if (option == 4)
+            {
+                Console.WriteLine("Quiting the game...");
+            } else
+            {
+                Console.WriteLine("Error: Invalid option entered");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Thank you for playing");
+            Console.WriteLine();
         }
     }
 }
